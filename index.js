4be0aa8bcf7d8e700b93a4f0029cac2a0e8be232
mshipan <mshipan657@gmail.com>
2023-06-30 21:52:26 +0600

    make usersCollection and initialize some user apis

--- index.js
+++ index.js
@@ -25,6 +25,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("travlerzDB").collection("users");
+
+    // users Apis
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const alreadyAUser = await usersCollection.findOne(query);
+      if (alreadyAUser) {
+        return res.send({ message: "User Already Exist" });
+      }
+      user.role = "user";
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
