daea3266fc3d36111accfaf61cb478c095bc4305
mshipan <mshipan657@gmail.com>
2023-07-05 23:17:09 +0600

    initialize user deletion api functionality

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // packages apis
     // view all packages
     app.get("/packages", async (req, res) => {
