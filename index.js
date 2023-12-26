3105dd0581cf2fe66403f454cee50dab865f2876
Shipan Mallik <mshipan657@gmail.com>
2023-12-26 23:46:49 +0600

    update corsConfig

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  // methods: ["GET", "POST", "PUT", "DELETE"],
+  optionSuccessStatus: 200,
+};
+
 //middlewares
-app.use(cors());
+app.use(cors(corsConfig));
+app.options("", cors(corsConfig));
 app.use(express.json());
 
 //mongodb start
